d23412f4f285fff9de8af1c1bb1bb2a273c2fde8
Jack Murray <jack@murray.software>
2023-09-27 08:19:57 +0000

    Add some more customer types, revamp the style a bit

--- beatmatcher/models.py
+++ beatmatcher/models.py
@@ -4,8 +4,12 @@ from django.db import models
 class Interest(models.Model):
     TYPE_CHOICES = [
         ("dj", "DJ"),
+        ("band", "Band"),
+        ("act", "Live act"),
         ("venue", "Venue"),
+        ("festival", "Festival"),
+        ("event", "Event organizer"),
     ]
-    type = models.CharField(max_length=5, choices=TYPE_CHOICES)
+    type = models.CharField(max_length=200, choices=TYPE_CHOICES)
     name = models.CharField(max_length=200)
     email = models.EmailField()
