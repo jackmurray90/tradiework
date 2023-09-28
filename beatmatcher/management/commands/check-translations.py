1addc6993781892319754ea42da37eba98bfd374
Jack Murray <jack@murray.software>
2023-09-29 09:42:36 +1000

    improve check-translations script

--- beatmatcher/management/commands/check-translations.py
+++ beatmatcher/management/commands/check-translations.py
@@ -10,9 +10,10 @@ class Command(BaseCommand):
         trs = [tr.split(".")[1] for tr in trs.decode("utf-8").split("\n") if tr]
         trs = set(trs)
         for lang in tr:
-            if len(tr[lang]) == len(trs):
+            missing = False
+            for string in trs:
+                if string not in tr[lang]:
+                    print(lang, "is missing", string)
+                    missing = True
+            if not missing:
                 print(lang, "has all translations")
-            else:
-                for string in trs:
-                    if string not in tr[lang]:
-                        print(lang, "is missing", string)
