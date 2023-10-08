8223c62e0741c48022a95715373c786ee1636ab3
Jack Murray <jack@murray.software>
2023-10-08 21:01:14 +1100

    refactor app/emails.py

--- app/emails.py
+++ app/emails.py
@@ -11,8 +11,10 @@ def send_sign_up_email(request, language, email, code):
     html = get_template(f"emails/sign-up.html")
     subject = tr("Welcome to %s", language) % settings.SITE_TITLE
     from_email = f"no-reply@{request.get_host()}"
-    text_content = plaintext.render({f"language": language, f"url": request.build_absolute_uri(reverse(f"sign-up-verify", kwargs={f"code": code}))})
-    html_content = html.render({f"language": language, f"url": request.build_absolute_uri(reverse(f"sign-up-verify", kwargs={f"code": code}))})
+    url = request.build_absolute_uri(reverse(f"sign-up-verify", kwargs={f"code": code}))
+    context = {f"language": language, f"url": url}
+    text_content = plaintext.render(context)
+    html_content = html.render(context)
 
     # Create the email message with the content and send it
     message = EmailMultiAlternatives(subject, text_content, from_email, [email])
