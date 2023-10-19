c854769f2d0d07f3a90e97314163ff8ed6af1fb9
Jack Murray <jack@murray.software>
2023-10-19 12:29:56 +1100

    add logging

--- app/settings.py
+++ app/settings.py
@@ -156,3 +156,22 @@ STATICFILES_DIRS = [BASE_DIR / "assets"]
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        "file": {
+            "level": "DEBUG",
+            "class": "logging.FileHandler",
+            "filename": "debug.log",
+        },
+    },
+    "loggers": {
+        "django": {
+            "handlers": ["file"],
+            "level": "DEBUG",
+            "propagate": True,
+        },
+    },
+}
